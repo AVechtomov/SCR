5f2c0169af9f7ab3ea45f12d69f02c23ebe34063
AVechtomov <vechtomov.alexandr@yandex.ru>
2015-04-06 19:10:53 +0400

    start

---
+++ webserver/webserver.java
@@ -0,0 +1,7 @@
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
