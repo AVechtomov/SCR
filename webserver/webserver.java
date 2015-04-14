729cef1e78ef2938ed0fe57aeff943675a78599e
AVechtomov <vechtomov.alexandr@yandex.ru>
2015-04-15 01:33:39 +0400

    serverlet

--- webserver/webserver.java
+++ webserver/webserver.java
@@ -1,7 +1,27 @@
-
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+ 
+public class MyServlet extends HttpServlet {
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+ 
+      // установить MIME-type и кодировку ответа
+      response.setContentType("text/html; charset=UTF8");
+      PrintWriter out = response.getWriter();
+ 
+      // Отправка веб-страницы
+		try {
+         out.println("<html>");
+         out.println("<head><title>Servlet sample</title></head>");
+         out.println("<body>");
+         out.println("<p>Запрошенный ресурс: " + request.getRequestURI() + "</p>");
+         out.println("<p>Протокол: " + request.getProtocol() + "</p>");
+         out.println("<p>Адрес сервера: " + request.getRemoteAddr() + "</p>");
+         out.println("</body></html>");
+      } finally {
+         out.close();  // Всегда закрывать Writer
+      }
+   }
+}
